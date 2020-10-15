541edc1aca9c407aa2f3e83f8c086475ce832298
Hlib Kavraskyi <h.kavraskyi@amoniac.eu>
2020-10-15 19:59:00 +0300

    Config project

---
+++ src/components/SodaMachine/SodaMachine.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+.soda-machine
+</template>
---
+++ src/views/Hall.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="hall">
+    <SodaMachine />
+  </div>
+</template>
+
+<script>
+import SodaMachine from "@/components/SodaMachine/SodaMachine.vue";
+
+export default {
+  name: "Hall",
+  components: {
+    SodaMachine
+  }
+};
+</script>
